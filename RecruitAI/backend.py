c64c2f0d2ac79034919f732af6b1029e2097fce4
Ritik M <ritikm3579@gail.com>
2025-04-28 23:33:11 +0530

    -

--- RecruitAI/backend.py
+++ RecruitAI/backend.py
@@ -27,9 +27,6 @@ from docx import Document
 from pypdf import PdfReader
 from dotenv import load_dotenv
 
-load_dotenv()
-
-api_key = os.getenv("OPENAI_API_KEY")
 
 # ======================================================================================================
 #                                       logging configuration       
